177814bd2a2f41b8941ce3f632bce24f2d83fff0
Yosua <godbless_yosua@yahoo.co.id>
2023-10-29 01:05:47 +0700

    update control

--- control.js
+++ control.js
@@ -1,117 +1,117 @@
 var openlpChannel = new BroadcastChannel("obs_openlp_channel");
 
 window.OpenLP = {
-    updateServiceList : function() {
-        $.getJSON("/api/service/list").then( (data, status) => {
-            var $serviceList = $("#service-list").empty();
-            data.results.items.forEach( (item, i) => {
-                var { id, title, selected } = item;
-                var $item = $(`<div class="service-item${selected ? " selected" : ""}"/>`);
-                $item.html(title)
-                    .click( () => {
-                        $.post(`/api/service/set?data={"request": {"id": ${i}}}`);
-                    });
-
-                $serviceList.append($item);            
-            });
+  updateServiceList: function () {
+    $.getJSON("/api/service/list").then((data, status) => {
+      var $serviceList = $("#service-list").empty();
+      data.results.items.forEach((item, i) => {
+        var { id, title, selected } = item;
+        var $item = $(
+          `<div class="service-item${selected ? " selected" : ""}"/>`
+        );
+        $item.html(title).click(() => {
+          $.get(`/api/service/set?data={"request": {"id": ${i}}}`);
         });
-    },
-    updateTextList: function(newSlide) {
-        $.getJSON("/api/controller/live/text").then( (data, status) => {
-            this.currentSlides = data.results.slides;
-            this.currentRows = [];
 
-            var $textList = $("#text-list").empty();
-            data.results.slides.forEach( (slide, i) => {
-                var { text } = slide;
+        $serviceList.append($item);
+      });
+    });
+  },
+  updateTextList: function (newSlide) {
+    $.getJSON("/api/controller/live/text").then((data, status) => {
+      this.currentSlides = data.results.slides;
+      this.currentRows = [];
 
-                var rows = this.slideToRows(text);
-                rows.forEach( rowText => {
-                    var row = this.currentRows.length;
+      var $textList = $("#text-list").empty();
+      data.results.slides.forEach((slide, i) => {
+        var { text } = slide;
 
-                    if(this.currentRow === null && this.currentSlide === i) {
-                        this.currentRow = row;
-                    }
+        var rows = this.slideToRows(text);
+        rows.forEach((rowText) => {
+          var row = this.currentRows.length;
 
-                    var selected = row === this.currentRow;
-                    this.currentRows.push(rowText);
+          if (this.currentRow === null && this.currentSlide === i) {
+            this.currentRow = row;
+          }
 
-                    if(selected) {
-                        openlpChannel.postMessage(rowText);
-                        var $live = $("#live").empty().html(rowText);
-                    }
+          var selected = row === this.currentRow;
+          this.currentRows.push(rowText);
 
-                    var $row = $(`<div class="text-item${selected ? " selected" : ""}"/>`);
-                    $row.html(rowText)
-                        .click( () => {
-                            this.currentRow = row;
-                            if(this.currentSlide !== i) {
-                                this.currentSlide = i;
-                                $.post(`/api/controller/live/set?data={"request": {"id": ${i}}}`);
-                            }
-                            this.updateTextList();
-                        });
+          if (selected) {
+            openlpChannel.postMessage(rowText);
+            var $live = $("#live").empty().html(rowText);
+          }
 
-                    $textList.append($row);            
-                });
-
-
-            });
-        })
-    },
-    slideToRows : function(slideText){
-        var rows = [];
-
-        if(this.modeTampilan === "original"){
-            rows.push(slideText.replace("\n", "<br>"));
-        }
-        else if(this.modeTampilan === "2-line") {
-            var lines = slideText.split("\n");
-            for (var i = 0; i < lines.length; i+=2) {
-                var nextLine = lines[i+1];
-                nextLine = nextLine ? ("<br>" + nextLine) : "";
-                var row = lines[i] + nextLine;
-                rows.push(row);
+          var $row = $(
+            `<div class="text-item${selected ? " selected" : ""}"/>`
+          );
+          $row.html(rowText).click(() => {
+            this.currentRow = row;
+            if (this.currentSlide !== i) {
+              this.currentSlide = i;
+              $.get(`/api/controller/live/set?data={"request": {"id": ${i}}}`);
             }
-        }
-        return rows;
-    },
-    pollServer : function() {
-        $.getJSON("/api/poll").then( (data, status) => {
-            if (this.currentItem != data.results.item ||
-                this.currentService != data.results.service) {
-                this.currentItem = data.results.item;
-                this.currentService = data.results.service;
-                this.currentSlide = 0;
-                this.currentRow = 0;
+            this.updateTextList();
+          });
 
-                this.updateServiceList();
-                this.updateTextList();
-            } else if (this.currentSlide != data.results.slide) {
-                this.currentSlide = parseInt(data.results.slide, 10);
-                this.currentRow = null;
-                this.updateTextList();
-            }
+          $textList.append($row);
         });
-    },
-    setModeTampilan: function(mode) {
+      });
+    });
+  },
+  slideToRows: function (slideText) {
+    var rows = [];
+
+    if (this.modeTampilan === "original") {
+      rows.push(slideText.replace("\n", "<br>"));
+    } else if (this.modeTampilan === "2-line") {
+      var lines = slideText.split("\n");
+      for (var i = 0; i < lines.length; i += 2) {
+        var nextLine = lines[i + 1];
+        nextLine = nextLine ? "<br>" + nextLine : "";
+        var row = lines[i] + nextLine;
+        rows.push(row);
+      }
+    }
+    return rows;
+  },
+  pollServer: function () {
+    $.getJSON("/api/poll").then((data, status) => {
+      if (
+        this.currentItem != data.results.item ||
+        this.currentService != data.results.service
+      ) {
+        this.currentItem = data.results.item;
+        this.currentService = data.results.service;
         this.currentSlide = 0;
         this.currentRow = 0;
-        this.modeTampilan = mode;
-        $(`input[name="mode-tampilan"][value="${mode}"]`).prop('checked', true);
+
+        this.updateServiceList();
         this.updateTextList();
-    }
-}
+      } else if (this.currentSlide != data.results.slide) {
+        this.currentSlide = parseInt(data.results.slide, 10);
+        this.currentRow = null;
+        this.updateTextList();
+      }
+    });
+  },
+  setModeTampilan: function (mode) {
+    this.currentSlide = 0;
+    this.currentRow = 0;
+    this.modeTampilan = mode;
+    $(`input[name="mode-tampilan"][value="${mode}"]`).prop("checked", true);
+    this.updateTextList();
+  },
+};
 
 $(function () {
-    $.ajaxSetup({cache: false});
-    setInterval( () => {
-        OpenLP.pollServer();
-    }, 250);
+  $.ajaxSetup({ cache: false });
+  setInterval(() => {
+    OpenLP.pollServer();
+  }, 250);
 
-    OpenLP.setModeTampilan("original");
-    $('input[type=radio][name="mode-tampilan"]').change( (evt) => {
-        OpenLP.setModeTampilan(evt.target.value);
-    });
+  OpenLP.setModeTampilan("original");
+  $('input[type=radio][name="mode-tampilan"]').change((evt) => {
+    OpenLP.setModeTampilan(evt.target.value);
+  });
 });
-
